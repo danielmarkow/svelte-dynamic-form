d9a8a1e9dcaf3037b5934fd42caaf6dc379b1fd1
Daniel Markow <d_markow@icloud.com>
2023-09-26 19:44:22 +0200

    saving form results

--- src/routes/public/[formId]/+page.server.ts
+++ src/routes/public/[formId]/+page.server.ts
@@ -1,15 +1,37 @@
 import { client } from '$lib/db/mongo';
 import { ObjectId } from 'mongodb';
 import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+
+export const actions = {
+	result: async ({ request, params }) => {
+		const data = await request.formData();
+		const formRes = Object.fromEntries(data);
+		try {
+			await client
+				.db('dynForms')
+				.collection('results')
+				.insertOne({ formId: params.formId, ...formRes });
+		} catch (error) {
+			console.error(error);
+			return fail(500, { message: 'error saving to db' });
+		}
+	}
+};
 
 export const load = (async ({ params }) => {
-	const publicForm = await client
-		.db('dynForms')
-		.collection('forms')
-		.findOne({ _id: new ObjectId(params.formId) }, { projection: { _id: 0 } });
+	try {
+		const publicForm = await client
+			.db('dynForms')
+			.collection('forms')
+			.findOne({ _id: new ObjectId(params.formId) }, { projection: { _id: 0 } });
 
-	if (publicForm && publicForm.public === false) {
-		return { formId: params.formId };
+		if (publicForm && publicForm.public === false) {
+			return { formId: params.formId };
+		}
+		return { formId: params.formId, form: publicForm };
+	} catch (error) {
+		console.error(error);
+		return fail(500, { message: 'error fetching form db' });
 	}
-	return { formId: params.formId, form: publicForm };
 }) satisfies PageServerLoad;
--- src/routes/public/[formId]/+page.svelte
+++ src/routes/public/[formId]/+page.svelte
@@ -1,9 +1,44 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
-<p>test</p>
 {data.formId}
 {JSON.stringify(data.form)}
+
+{#if data.form}
+	<div class="h-4" />
+	<h1 class="text-xl">{data.form.name}</h1>
+	<p>{data.form.description}</p>
+	<div class="h-4" />
+	<form method="POST" action="?/result" use:enhance>
+		<div class="flex flex-col gap-y-1 md:w-2/3 w-full">
+			{#if data.form.elements.length === 0}
+				<p class="italic">no form elements yet</p>
+			{/if}
+			{#each data.form.elements as el}
+				{#if el.type === 'input'}
+					<div id={el.id} class="flex flex-col">
+						<label for={el.id}>{el.label}</label>
+						<input id={el.id} type={el.args} name={el.name} required={el.required || null} />
+					</div>
+				{/if}
+				{#if el.type === 'select'}
+					<div id={el.id} class="flex flex-col gap-y-1">
+						<label for={el.id}>{el.label}</label>
+						<select id={el.id} name={el.name}>
+							{#each el.args as opt}
+								<option value={opt.optValue}>{opt.optLabel}</option>
+							{/each}
+						</select>
+					</div>
+				{/if}
+			{/each}
+		</div>
+		<button formaction="?/result">send</button>
+	</form>
+{:else}
+	<p>form not public</p>
+{/if}
